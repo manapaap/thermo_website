8d1ec9f78f06bd930cdcbe49fd98605a58969ba8
Aakash Manapat <aakash.p.manapat@vanderbilt.edu>
2022-05-31 21:29:35 -0500

    The web app is working right now!

---
+++ eos_solver.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 17:32:54 2022
+
+@author: Aakas
+"""
+
+import thermo.eos as eos
+import numpy as np
+
+
+gas_constant = 8.31446  # J/mol K
+
+
+def solve_eos(eqn_state, relevant_vals):
+    """
+    Solves a cubic equation of state by redirecting to the relevant equation
+    of state and then returns the values we cant to calculate.
+
+    Returns a dictionary containing departure functions and the roots for the
+    cubic EOS
+    """
+    if eqn_state == 'vdW':
+        solutions = eos.VDW(**relevant_vals)
+    elif eqn_state == 'RK':
+        solutions = eos.RK(**relevant_vals)
+    elif eqn_state == 'SRK':
+        solutions = eos.SRK(**relevant_vals)
+    else:
+        solutions = eos.PR(**relevant_vals)
+
+    departs, roots = process_solutions(solutions)
+    return departs, roots
+
+
+def fugacity(del_g_dep, temp):
+    """
+    Baby function that returns fugacity using ΔGdep
+    """
+    return np.exp(del_g_dep / (temp * gas_constant))
+
+
+def pass_liquid_vals(solved_eos, liquid_depart, roots):
+    """
+    Does the manual work of taking the solved EOS and adding the
+    calculated values to it couldn't think of a cleaner way to do it
+    """
+    liquid_depart['Δhdep'] = solved_eos.H_dep_l
+    liquid_depart['Δudep'] = solved_eos.U_dep_l
+    liquid_depart['Δsdep'] = solved_eos.S_dep_l
+    liquid_depart['φ'] = fugacity(solved_eos.G_dep_l, solved_eos.T)
+
+    roots['liquid'] = solved_eos.V_l
+
+    return liquid_depart, roots
+
+
+def pass_vapor_vals(solved_eos, vapor_depart, roots):
+    """
+    Does the manual work of taking the solved EOS and adding the
+    calculated values to it couldn't think of a cleaner way to do it
+    """
+    vapor_depart['Δhdep'] = solved_eos.H_dep_g
+    vapor_depart['Δudep'] = solved_eos.U_dep_g
+    vapor_depart['Δsdep'] = solved_eos.S_dep_g
+    vapor_depart['φ'] = fugacity(solved_eos.G_dep_g, solved_eos.T)
+
+    roots['vapor'] = solved_eos.V_g
+
+    return vapor_depart, roots
+
+
+def process_solutions(solved_eos):
+    """
+    Processes the solutions of the EOS into a nice format for easy printing
+    """
+    liquid_depart = {}
+    vapor_depart = {}
+    roots = {}
+    liquid_depart, roots = pass_liquid_vals(solved_eos, liquid_depart, roots)
+    vapor_depart, roots = pass_vapor_vals(solved_eos, vapor_depart, roots)
+
+    departs = {'liquid': liquid_depart,
+               'vapor': vapor_depart}
+
+    return departs, roots
--- main_host.py
+++ main_host.py
@@ -3,6 +3,9 @@
 Created on Mon May 30 13:42:33 2022
 
 @author: Aakas
+
+Thermo Website: this script hosts the main section of the website, but
+relies upon other scripts to perform the actual math
 """
 
 from os import chdir
@@ -13,6 +16,7 @@ from sys import platform
 
 if platform == 'win32':
     chdir('C:/Users/Aakas/Documents/School/CHBE_2250/')
+from eos_solver import solve_eos
 
 
 def equation_of_state(column):
@@ -76,8 +80,8 @@ def molecule(column):
             (['Custom'] + options))
 
         st.write('If using custom values')
-        cr_temp = st.number_input(f'T{get_sub("c")}')
-        cr_pres = st.number_input(f'P{get_sub("c")}')
+        cr_temp = st.number_input(f'T{get_sub("c")} (K)')
+        cr_pres = st.number_input(f'P{get_sub("c")} (bar)')
         accen_fac = st.number_input('ω')
 
         if chosen_molecule == 'Custom':
@@ -97,20 +101,21 @@ def conditions(column):
     """
     with column:
         st.write('Define a state of interest')
-        temp = st.number_input('Temperature')
-        pres = st.number_input('Pressure')
+        temp = st.number_input('Temperature (K)')
+        pres = st.number_input('Pressure (bar)')
 
         return temp, pres
 
 
-def roots(column, roots=['']):
+def roots(column, roots):
     """
     Displays the roots of the cubic EOS, corresponding to volume
     """
     with column:
         st.write('Roots to the cubic EOS')
-        for root in roots:
-            st.write(f'v = {root} m{get_sup("3")} mol{get_sup("-1")}')
+        for key, value in roots.items():
+            st.write(f'{key.capitalize()} root:')
+            st.write(f'v = {value:.8f} m{get_sup("3")} mol{get_sup("-1")}')
 
 
 def departure_fxn(column, depart_vals, depart_units, depart_disp):
@@ -121,7 +126,10 @@ def departure_fxn(column, depart_vals, depart_units, depart_disp):
     """
     with column:
         for key, value in depart_vals.items():
-            st.write(f'{depart_disp[key]} = {value} {depart_units[key]}')
+            st.write(f'{key.capitalize()} departure functions:')
+            for important, worth in value.items():
+                st.write(f'{depart_disp[important]} = ' +
+                         f'{worth:.4f} {depart_units[important]}')
 
 
 def base_vals():
@@ -147,21 +155,43 @@ def base_vals():
     return depart_units, depart_base, depart_disp
 
 
+def process_inputs(chosen_eos, cr_temp, cr_pres, accen_fac, temp, pres):
+    """
+    Turns the various user inpute into a dictionary for easier passing and
+    handling by thermo eos library. Also rescales pressure to pascals
+    during the process for the same reason
+    """
+    if (cr_temp or cr_pres or accen_fac or temp or pres) < 0:
+        st.write('INVALID INPUTS: Please enter valid inputs')
+        return False
+
+    relevant_vals = {'Tc': cr_temp,
+                     'Pc': cr_pres * 1e5,
+                     'omega': accen_fac,
+                     'T': temp,
+                     'P': pres * 1e5}
+    return relevant_vals
+
+
 def main():
     depart_units, depart_base, depart_disp = base_vals()
 
     st.title("Aakash's Website for Solving Cubic Equations of State")
-    left_column, right_column = st.columns(2)
+    left_column, right_column, leftest_column = st.columns(3)
 
     chosen_eos = equation_of_state(left_column)
     cr_temp, cr_pres, accen_fac = molecule(right_column)
     temp, pres = conditions(left_column)
 
-    # Temporary setup to render accurately for now, need to add functionality
-    # later
     if left_column.button('Calculate!'):
-        roots(right_column)
-        departure_fxn(left_column, depart_base, depart_units, depart_disp)
+        relevant_vals = process_inputs(chosen_eos, cr_temp, cr_pres,
+                                       accen_fac, temp, pres)
+        if relevant_vals:
+            departs, volume_roots = solve_eos(chosen_eos, relevant_vals)
+            # right column
+            roots(leftest_column, volume_roots)
+            # left column
+            departure_fxn(leftest_column, departs, depart_units, depart_disp)
     else:
         st.write('')
 
